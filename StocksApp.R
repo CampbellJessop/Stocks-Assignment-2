becb560fa0427671c5bd2e9b97ad1390b92b19a1
CampbellJessop <camcjessop@gmail.com>
2022-03-10 10:26:15 -0500

    Update StocksApp.R to dashboard layout

--- StocksApp.R
+++ StocksApp.R
@@ -2,6 +2,7 @@ library(fpp3)
 library(readr)
 library(shinyWidgets)
 library(shiny)
+
 #shinyWidgetsGallery()
 
 # Read zipped data
@@ -11,61 +12,108 @@ stocks <- read_csv("nyse_stocks.csv.zip")
 stocks$date <- as.Date(stocks$date)
 stocks <- tsibble(stocks, index = date, key = symbol)
 
-# 1 stock
-selected_stock <- "AAPL"
-
-stocks %>%
-  filter(symbol == selected_stock) %>%
-  autoplot(open) +
-  labs(title = selected_stock)
-
-# Multiple stocks
-selected_stocks <- c("GOOG", "AAPL")
-
-stocks %>%
-  filter(symbol %in% selected_stocks) %>%
-  autoplot(open)
-
-?awesomeRadio()
+# # 1 stock
+# selected_stock <- "AAPL"
+# 
+# stocks %>%
+#   filter(symbol == selected_stock) %>%
+#   autoplot(open) +
+#   labs(title = selected_stock)
+# 
+# # Multiple stocks
+# selected_stocks <- c("GOOG", "AAPL")
+# 
+# stocks %>%
+#   filter(symbol %in% selected_stocks) %>%
+#   autoplot(open)
 
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Group Assignment 2 - Stocks!"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-     selectizeInput(inputId="stockselect",
-                     label="Select some stocks!",
-                     choices=unique(stocks$symbol),
-                     multiple=TRUE),
-     awesomeRadio(
-       inputId = "yvar",
-       label = "Select a variable to study:", 
-       choices = c("open","volume"),
-       selected = "open"
-     ),
-     dateInput("Start", label = h3("Starting Date"), value = "2010-01-01"),
-     dateInput("End", label = h3("Ending Date"), value = "2017-01-01")
+
+body <- dashboardBody(
+  fluidRow(
+    box(
+      title = "Stock Time Plot", 
+      status = "primary", 
+      solidHeader = TRUE,
+      collapsible = TRUE,
+      plotOutput("timePlot", 
+                 height = 250)
+    ),
     
-     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("timePlot"),
-      #Show summary of plot
+    box(
+      title = "Stock Text Output",
+      status = "warning",
+      solidHeader = TRUE,
+      collapsible = TRUE,
       verbatimTextOutput("summary")
+    )
+  ),
+  
+  
+  fluidRow(
+    box(
+      title = "Input #1",
+      status = "info",
+      solidHeader = TRUE,
+      width = 4,
+      selectizeInput(inputId="stockselect",
+                     label="Select some stocks:",
+                     choices=unique(stocks$symbol),
+                     multiple=TRUE)
+    ),
+    box(
+      title = "Input #2",
+      status = "success",
+      solidHeader = TRUE,
+      width = 4,
+      awesomeRadio(
+        inputId = "yvar",
+        label = "Select a variable to study:", 
+        choices = c("open","volume"),
+        selected = "open"
+      )
+    ),
+    box(
+      title = "Input #3", 
+      status = "danger",
+      solidHeader = TRUE,
+      width = 4,
+      dateInput("Start", label = "Select a starting date:", value = "2010-01-04"),
+      dateInput("End", label = "Select an ending date:", value = "2016-12-30")
+    )
+  ),
+  
+  fluidRow(
+    box(
+      width = 4, 
+      status = "info",
+      solidHeader = TRUE,
+      "Select as many stocks as you would like by searching its ticker symbol."
+    ),
+    box(
+      width = 4, 
+      status = "success",
+      solidHeader = TRUE,
+      "'Open' = Opening price point", br(), "'Volume' = Number of shares traded"
+    ),
+    box(
+      width = 4, 
+      status = "danger",
+      solidHeader = TRUE,
+      "Stock information ranges from 1/4/2010 - 12/30/2016"
     )
   )
 )
 
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+  dashboardHeader(title = "Stocks Assignment"),
+  dashboardSidebar(disable=TRUE),
+  body
+)
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  
   output$timePlot <- renderPlot({
     # draw the time plot with selected symbols and y var
     selected_stocks <- input$stockselect
@@ -73,20 +121,15 @@ server <- function(input, output) {
       filter(symbol %in% selected_stocks) %>%
       select(symbol,input$yvar) %>% 
       autoplot() + scale_x_date(limits = as.Date(c(input$Start,input$End)))
-    
-
-    
   })
   # Summary output of selected stocks per date range 
   output$summary<-renderPrint({
     selected_stocks <- input$stockselect
     stockss<-as.data.frame(stocks)
-x<-subset(stockss,subset=symbol%in%selected_stocks& date>as.Date(input$Start)&date<as.Date(input$End),select = c(symbol,open,volume))
-   tapply(x[,input$yvar],x$symbol,summary)
-    
-    
-    
+    x<-subset(stockss,subset=symbol%in%selected_stocks& date>as.Date(input$Start)&date<as.Date(input$End),select = c(symbol,open,volume))
+    tapply(x[,input$yvar],x$symbol,summary)
   })}
 
-# Run the application 
+
+# Preview the UI in the console
 shinyApp(ui = ui, server = server)
